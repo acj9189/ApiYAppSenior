985155a21ae94a1d88644cf49b5c3d8846bafeaf
acj9189 <acj8991@gmail.com>
2019-07-27 00:30:40 -0500

    Actualizacion casos de Prueba Api...

--- NUnitTestProjectApiAssistSenior/UnitTest1.cs
+++ NUnitTestProjectApiAssistSenior/UnitTest1.cs
@@ -4,13 +4,65 @@ namespace Tests
 {
     public class Tests
     {
+
+
         [SetUp]
         public void Setup()
         {
         }
 
+         /***
+         * Casos de Prueba para el metdo get de la Api...
+         */ 
+
         [Test]
-        public void Test1()
+        public void cadoPruebaGetEnfermeroApiEqual()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void cadoPruebaGetEnfermeroApiEqualNull()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void cadoPruebaGetEnfermeroApiException()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void cadoPruebaGetEnfermeroApiDatoNOValido()
+        {
+            Assert.Pass();
+        }
+
+         /***
+         * Casos de Prueba para el metdo put de la Api...
+         */
+
+        [Test]
+        public void cadoPruebaPutEnfermeroApiEqual()
+        {
+            Assert.Pass();
+        }
+ 
+        [Test]
+        public void cadoPruebaPutEnfermeroApiEqualNull()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void cadoPruebaPutEnfermeroApiException()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void cadoPruebaPutEnfermeroApiDatoNOValido()
         {
             Assert.Pass();
         }
